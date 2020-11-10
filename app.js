38bc8ce6d306765c7aa34b60659c60bb36162f81
DamnTime <386484304@qq.com>
2020-11-10 17:57:13 +0800

    feat: 初始化创建表

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ class AppBootHook {
   configWillLoad() {
     require('module-alias/register');
   }
-  async beforeStart() {
-    const isProd = process.env.NODE_ENV === 'production';
-    if (isProd) {
-      await this.app.model.sync({ force: true });
-    }
-  }
+  // async beforeStart() {
+  //   const isProd = process.env.NODE_ENV === 'production';
+  //   if (isProd) {
+  //     await this.app.model.sync({ force: true });
+  //   }
+  // }
 }
 
 module.exports = AppBootHook;
